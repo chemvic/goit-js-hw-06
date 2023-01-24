6d516186cd87d561f9443f93a841532c818685d5
chemvic <chemvic79@gmail.com>
2023-01-24 21:24:58 +0200

    Update task-02.js
    второе задание

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector("#ingredients");
+const ingredientsEl = [];
+ingredients.forEach(ingredient => {
+  const ingredientEl = document.createElement('li');
+ingredientEl.classList.add("item");
+  ingredientEl.textContent = ingredient;
+  ingredientsEl.push(ingredientEl);
+  });
+list.append(...ingredientsEl);
