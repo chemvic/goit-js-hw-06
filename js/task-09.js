2412342c8e8f269d4670c1f3a47f71e46e6aa121
chemvic <chemvic79@gmail.com>
2023-01-29 19:22:42 +0200

    Update task-09.js
    исправил чтобы меняло цвет постоянно

--- js/task-09.js
+++ js/task-09.js
@@ -3,7 +3,8 @@ const changeColorButton = document.querySelector('.change-color');
 const colorInSpan = document.querySelector('.color');
 
 
-let bodyBackGrColor = getRandomHexColor();
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16).padStart(6, 0)}`;
@@ -12,7 +13,7 @@ function getRandomHexColor() {
 changeColorButton.addEventListener('click', onButtonClickChangeColor);
 
 function onButtonClickChangeColor(event) {
- 
+ let bodyBackGrColor = getRandomHexColor();
   document.body.style.backgroundColor = bodyBackGrColor;
   colorInSpan.textContent = document.body.style.backgroundColor;
   
